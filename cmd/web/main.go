b6d27b7a72f91a96525501a84ce98bfe1c0db10a
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-12-13 22:34:10 -0300

    feat(wasm): implement main function and identify handler for WebAssembly integration

---
+++ cmd/web/main.go
@@ -0,0 +1,69 @@
+//go:build wasm && js
+
+package main
+
+import (
+	"syscall/js"
+
+	serve "github.com/alan-b-lima/nn-digits/internal/service"
+	"github.com/alan-b-lima/nn-digits/internal/service/wasm"
+)
+
+func main() {
+	serve := wasm.New()
+	js.Global().Set("identify", js.FuncOf(identify(serve)))
+	
+	select {}
+}
+
+var Array = js.Global().Get("Array")
+
+func identify(id serve.Identifier) func(js.Value, []js.Value) any {
+	return func(_ js.Value, args []js.Value) any {
+		var width, height int
+		var array []float64
+
+		var arg js.Value
+
+		if arg = args[1]; arg.Type() != js.TypeNumber {
+			return nil
+		}
+		width = arg.Int()
+
+		if arg = args[2]; arg.Type() != js.TypeNumber {
+			return nil
+		}
+		height = arg.Int()
+
+		if arg = args[0]; arg.Type() != js.TypeObject || !arg.InstanceOf(Array) {
+			return nil
+		}
+		length := arg.Get("length").Int()
+		array = make([]float64, length)
+
+		for i := range length {
+			val := arg.Index(i)
+			if val.Type() != js.TypeNumber {
+				return nil
+			}
+
+			array[i] = val.Float()
+		}
+
+		res, err := id.Identify(serve.Request{
+			Width:  width,
+			Height: height,
+			Data:   array,
+		})
+		if err != nil {
+			return nil
+		}
+
+		result := Array.New(len(res))
+		for i, val := range res {
+			result.SetIndex(i, val)
+		}
+
+		return result
+	}
+}
