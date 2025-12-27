07efc1c74b59ed55b7e80cf4ca6a7324ef711248
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-12-27 10:41:13 -0300

    refactor(cost): fix cost function and change cost derivative to sample cost derivative

--- internal/neural_network/cost.go
+++ internal/neural_network/cost.go
@@ -1,32 +1,28 @@
 package nn
 
+import "github.com/alan-b-lima/nn-digits/pkg/nnmath"
+
 func (nn *NeuralNetwork) Cost(dataset []LabeledSample) float64 {
 	var cost float64
 	for _, sample := range dataset {
 		output := nn.FeedForward(sample.Values).Data()
 		expected := sample.Label.Data()
 
-		var cost float64
 		for i := range len(output) {
 			diff := output[i] - expected[i]
 			cost += diff * diff
 		}
 	}
 
-	return cost / float64(len(dataset))
+	return .5 * cost / float64(len(dataset))
 }
 
-func (nn *NeuralNetwork) CostDerivative(dataset []LabeledSample) float64 {
-	var cost float64
-	for _, sample := range dataset {
-		output := nn.FeedForward(sample.Values).Data()
-		expected := sample.Label.Data()
+func (nn *NeuralNetwork) SampleCostDerivative(cost nnmath.Vector, sample LabeledSample) {
+	output := nn.FeedForward(sample.Values).Data()
+	expected := sample.Label.Data()
 
-		var cost float64
-		for i := range len(output) {
-			cost += output[i] - expected[i]
-		}
+	vector := cost.Data()
+	for i := range len(output) {
+		vector[i] += output[i] - expected[i]
 	}
-
-	return 2 * cost / float64(len(dataset))
 }
