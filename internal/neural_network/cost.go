15d6d89b6a5107e02b9cfa4ab97f9eee0484a919
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-12-27 10:41:59 -0300

    feat(cost): add Status method to compute accuracy and average cost

--- internal/neural_network/cost.go
+++ internal/neural_network/cost.go
@@ -17,6 +17,41 @@ func (nn *NeuralNetwork) Cost(dataset []LabeledSample) float64 {
 	return .5 * cost / float64(len(dataset))
 }
 
+func (nn *NeuralNetwork) Status(dataset []LabeledSample) (int, float64) {
+	var cost float64
+	var correct int
+
+	for _, sample := range dataset {
+		output := nn.FeedForward(sample.Values).Data()
+		expected := sample.Label.Data()
+
+		for i := range len(output) {
+			diff := output[i] - expected[i]
+			cost += diff * diff
+		}
+
+		index := reduce(output, func(index int, v float64, i int) int {
+			if v > output[index] {
+				return i
+			}
+			return index
+		})
+
+		label := reduce(expected, func(label int, v float64, i int) int {
+			if v > output[label] {
+				return i
+			}
+			return label
+		})
+
+		if index == label {
+			correct++
+		}
+	}
+
+	return correct, .5 * cost / float64(len(dataset))
+}
+
 func (nn *NeuralNetwork) SampleCostDerivative(cost nnmath.Vector, sample LabeledSample) {
 	output := nn.FeedForward(sample.Values).Data()
 	expected := sample.Label.Data()
@@ -25,4 +60,13 @@ func (nn *NeuralNetwork) SampleCostDerivative(cost nnmath.Vector, sample Labeled
 	for i := range len(output) {
 		vector[i] += output[i] - expected[i]
 	}
+}
+
+func reduce[E, R any](s []E, fn func(acc R, v E, i int) R) R {
+	var acc R
+	for i, v := range s {
+		acc = fn(acc, v, i)
+	}
+
+	return acc
 }
