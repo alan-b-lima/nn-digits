535d4df8380dc3343dcd24f0f9a064486e616f33
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-12-27 10:47:43 -0300

    feat(nn): restructure learning process to enhance gradient computation and implement backpropagation

--- internal/neural_network/training.go
+++ internal/neural_network/training.go
@@ -4,13 +4,41 @@ import (
 	"github.com/alan-b-lima/nn-digits/pkg/nnmath"
 )
 
-func (nn *NeuralNetwork) Learn(dataset []LabeledSample, rate float64) {
-	cost := nn.Cost(dataset)
-	const h = 1e-3
+const use_backpropagation = true
 
-	for _, layer := range nn.layers {
+func (nn *NeuralNetwork) Learn(dataset []LabeledSample, rate float64) {
+	nn.compute_gradient(dataset)
+	nn.apply_gradient(rate)
+}
+
+func (nn *NeuralNetwork) apply_gradient(rate float64) {
+	for _, layer := range nn.Layers {
+		nnmath.SMulP(layer.WeightGradient, -rate, layer.WeightGradient)
+		nnmath.SMulP(layer.BiasGradient, -rate, layer.BiasGradient)
+
+		nnmath.AddP(layer.Weights, layer.Weights, layer.WeightGradient)
+		nnmath.AddP(layer.Biases, layer.Biases, layer.BiasGradient)
+	}
+}
+
+func (nn *NeuralNetwork) compute_gradient(dataset []LabeledSample) {
+	if use_backpropagation {
+		nn.backpropagate(dataset)
+	} else {
+		nn.naive_brute_force(dataset)
+	}
+}
+
+func (nn *NeuralNetwork) naive_brute_force(dataset []LabeledSample) {
+	const h = 1e-3
+	cost := nn.Cost(dataset)
+
+	for _, layer := range nn.Layers {
 		weights := layer.Weights
-		gweight := layer.weight_gradient
+		biases := layer.Biases
+
+		gweight := layer.WeightGradient
+		gbias := layer.BiasGradient
 
 		for r := range weights.Rows() {
 			for c := range weights.Cols() {
@@ -20,12 +48,7 @@ func (nn *NeuralNetwork) Learn(dataset []LabeledSample, rate float64) {
 
 				gweight.Set(r, c, diff/h)
 			}
-		}
 
-		biases := layer.Weights
-		gbias := layer.bias_gradient
-
-		for r := range biases.Rows() {
 			biases.Set(r, 0, biases.At(r, 0)+h)
 			diff := nn.Cost(dataset) - cost
 			biases.Set(r, 0, biases.At(r, 0)-h)
@@ -33,12 +56,62 @@ func (nn *NeuralNetwork) Learn(dataset []LabeledSample, rate float64) {
 			gbias.Set(r, 0, diff/h)
 		}
 	}
+}
 
-	for _, layer := range nn.layers {
-		nnmath.SMulP(layer.weight_gradient, -rate, layer.weight_gradient)
-		nnmath.SMulP(layer.bias_gradient, -rate, layer.bias_gradient)
+func (nn *NeuralNetwork) backpropagate(dataset []LabeledSample) {
+	if len(nn.Layers) == 0 {
+		return
+	}
 
-		nnmath.AddP(layer.Weights, layer.Weights, layer.weight_gradient)
-		nnmath.AddP(layer.Biases, layer.Biases, layer.bias_gradient)
+	for _, layer := range nn.Layers {
+		nnmath.Zero(layer.WeightGradient)
+		nnmath.Zero(layer.BiasGradient)
+	}
+
+	for _, sample := range dataset {
+		{
+			input := sample.Values
+			if len(nn.Layers) > 1 {
+				input = nn.Layers[len(nn.Layers)-2].Activation
+			}
+
+			curr := nn.Layers[len(nn.Layers)-1]
+
+			nn.SampleCostDerivative(curr.ErrorPropagation, sample)
+
+			nnmath.ApplyP(curr.Activation, curr.Activation, SigmoidDerivativeFromActivation)
+			nnmath.HMulP(curr.ErrorPropagation, curr.ErrorPropagation, curr.Activation)
+
+			a := nnmath.MakeMatData(1, input.Rows(), input.Data())
+			nnmath.AddMulP(curr.WeightGradient, curr.WeightGradient, curr.ErrorPropagation, a)
+			nnmath.AddP(curr.BiasGradient, curr.BiasGradient, curr.ErrorPropagation)
+		}
+
+		for i := len(nn.Layers) - 2; i >= 0; i-- {
+			input := sample.Values
+			if i > 0 {
+				input = nn.Layers[i-1].Activation
+			}
+
+			next := nn.Layers[i+1]
+			curr := nn.Layers[i]
+
+			t := nnmath.MakeMatData(1, next.ErrorPropagation.Rows(), next.ErrorPropagation.Data())
+			r := nnmath.MakeMatData(1, curr.ErrorPropagation.Rows(), curr.ErrorPropagation.Data())
+			nnmath.MulP(r, t, next.Weights)
+
+			nnmath.ApplyP(curr.Activation, curr.Activation, SigmoidDerivativeFromActivation)
+			nnmath.HMulP(curr.ErrorPropagation, curr.ErrorPropagation, curr.Activation)
+
+			a := nnmath.MakeMatData(1, input.Rows(), input.Data())
+			nnmath.AddMulP(curr.WeightGradient, curr.WeightGradient, curr.ErrorPropagation, a)
+			nnmath.AddP(curr.BiasGradient, curr.BiasGradient, curr.ErrorPropagation)
+		}
+
+		factor := 1 / float64(len(dataset))
+		for _, layer := range nn.Layers {
+			nnmath.SMulP(layer.WeightGradient, factor, layer.WeightGradient)
+			nnmath.SMulP(layer.BiasGradient, factor, layer.BiasGradient)
+		}
 	}
 }
