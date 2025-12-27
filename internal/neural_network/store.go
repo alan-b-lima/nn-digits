91a190840343554db1a27da4cd98a3a62471489f
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-12-27 10:42:49 -0300

    refactor(store): apply mem package functionalities

--- internal/neural_network/store.go
+++ internal/neural_network/store.go
@@ -1,11 +1,9 @@
 package nn
 
 import (
-	"encoding/base64"
-	"encoding/binary"
 	"encoding/json"
-	"errors"
-	"math"
+
+	"github.com/alan-b-lima/nn-digits/pkg/mem"
 )
 
 func (nn NeuralNetwork) MarshalJSON() ([]byte, error) {
@@ -39,40 +37,6 @@ func (nn *NeuralNetwork) UnmarshalJSON(buf []byte) error {
 }
 
 type neural_network struct {
-	Dimensions []int `json:"dimensions"`
-	Layers     nums  `json:"layers"`
-}
-
-type nums []float64
-
-func (m nums) MarshalJSON() ([]byte, error) {
-	var bytes []byte
-	for _, n := range m {
-		bytes = binary.LittleEndian.AppendUint64(bytes, math.Float64bits(n))
-	}
-
-	buf := base64.StdEncoding.AppendEncode([]byte{'"'}, bytes)
-	buf = append(buf, '"')
-
-	return buf, nil
-}
-
-func (m *nums) UnmarshalJSON(buf []byte) error {
-	if len(buf) < 2 || buf[0] != '"' || buf[len(buf)-1] != '"' {
-		return errors.New("matrix must be a well-formed JSON string")
-	}
-
-	bytes, err := base64.StdEncoding.AppendDecode(nil, buf[1:len(buf)-1])
-	if err != nil {
-		return err
-	}
-
-	*m = make(nums, 0, len(bytes)/8)
-	for len(bytes) > 0 {
-		n := binary.LittleEndian.Uint64(bytes)
-		*m = append(*m, math.Float64frombits(n))
-		bytes = bytes[8:]
-	}
-
-	return nil
+	Dimensions []int            `json:"dimensions"`
+	Layers     mem.Float64Slice `json:"layers"`
 }
