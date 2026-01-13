cab1e3d4c473cf6cdcee750cacba2a1d15e29197
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2026-01-13 15:47:21 -0300

    refactor(nn)!: remove simple sample and rename labeled sample to sample

--- internal/neural_network/sample.go
+++ internal/neural_network/sample.go
@@ -3,10 +3,6 @@ package nn
 import "github.com/alan-b-lima/nn-digits/pkg/nnmath"
 
 type Sample struct {
-	Values nnmath.Vector
-}
-
-type LabeledSample struct {
 	Label  nnmath.Vector
 	Values nnmath.Vector
 }
