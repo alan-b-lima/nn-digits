0dfdb192812787c482a65147f4bbe8923e105380
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2026-01-13 15:45:53 -0300

    refactor(nn)!: move Softmax and its derivative to nn package

--- internal/neural_network/activation.go
+++ internal/neural_network/activation.go
@@ -14,3 +14,25 @@ func SigmoidDerivative(x float64) float64 {
 func SigmoidDerivativeFromActivation(x float64) float64 {
 	return x * (1 - x)
 }
+
+func Softmax(values []float64) []float64 {
+	var sum float64
+	for i := range len(values) {
+		values[i] = math.Exp(values[i])
+		sum += values[i]
+	}
+
+	for i := range len(values) {
+		values[i] /= sum
+	}
+
+	return values
+}
+
+func SoftmaxDerivativeFromActivation(values []float64) []float64 {
+	for i := range len(values) {
+		values[i] = values[i] * (1 - values[i])
+	}
+
+	return values
+}
